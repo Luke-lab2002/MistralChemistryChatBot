735185f9bc8dbaa62b23610e7289b7af51a783bb
Luke-lab2002 <nguyenvuminhcuong2002@gmail.com>
2024-12-12 12:20:37 +0700

    import dataset

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/', methods=["GET"])
 def hello_world():  # put application's code here
     return jsonify({"message": "Hello World!"})
@@ -14,17 +13,10 @@ def hello_world():  # put application's code here
 @app.route('/predict', methods=["POST"])
 def predict():
     data = request.get_json()
-    #
     conversation_content = data["conversation"]
-    print(conversation_content)
-
-
-
     result = generation(conversation_content)
 
     return jsonify({"message": result})
-
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=os.getenv("PORT", 5000))
